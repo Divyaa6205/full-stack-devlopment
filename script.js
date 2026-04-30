412bcc86395565a58abed3e18b4be39677eafd0b
DIVYA KUMARI <divya85390@gmail.com>
2026-04-30 22:55:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function showMessage() {
   alert("Welcome to MainCrafts 🚀");
 }
+
+function validateForm() {
+let name = document.getElementById("name").value;
+let email = document.getElementById("email").value;
+
+if(name==="" || email===""){
+alert("Fill all fields!");
+return false;
+}
+return true;
+}
